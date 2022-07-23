2f0f2cdfcab98de63b1e0a65f91fad8f6560f32f
mjombanorman <mjomban@gmail.com>
2022-07-23 16:24:00 +0300

    ML Microservices project intialization

---
+++ hadolint.sh
@@ -0,0 +1,3 @@
+sudo wget https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64 -O /usr/local/bin/hadolint
+sudo  chmod +x /usr/local/bin/hadolint
+exit 0
---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build -t mjomban/mlapp .
+
+# Step 2: 
+# List docker images
+docker images
+
+# Step 3: 
+# Run flask app
