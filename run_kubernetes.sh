f6f6d0d46b4a2ba12d3f7186a4988c3849f087e4
mjombanorman <mjomban@gmail.com>
2022-07-24 12:14:56 +0300

    Configure .sh scripts

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mjomban/mlapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapp --image=$dockerpath --port=80
+kubectl run mlapp --image=mjomban/mlapp --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlapp 80:80
